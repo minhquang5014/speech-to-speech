9ace140c319905c132f1531b60df176643884ffb
minhquang5014 <minhquang5014@gmail.com>
2025-01-06 21:45:55 +0700

    now, testing the STT models

--- STT/faster_whisper_handler.py
+++ STT/faster_whisper_handler.py
@@ -22,23 +22,30 @@ class FasterWhisperSTTHandler(BaseHandler):
         model_name: str = "tiny.en",
         device: str = "auto",
         compute_type: str = "auto",
-        gen_kwargs={},
+        gen_kwargs={},  # additional keyword arguments for the model
     ):
         self.gen_kwargs = self.adapt_gen_kwargs(gen_kwargs)
 
-        os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+        os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"  # rsolves conflicts with OpenMP when multiple models are loaded
+        
+        # loading the model FasterWhisper is instantiated with the given parameters
         self.model = WhisperModel(model_name, device=device, compute_type=compute_type)
 
     def process(self, audio):
-        logger.debug("infering faster whisper...")
-
+        """
+        Process the audio and return the transcribed text.
+        """
         global pipeline_start
+        
+        # to measure performance using perf_counter to track the pipeline's runtime
         pipeline_start = perf_counter()
 
+        # the transcibe function, passing the audio and any configured parameters
         segments, info = self.model.transcribe(audio, **self.gen_kwargs)
         output_text = []
 
         for segment in segments:
+            # group all the segments into a full text
             logger.debug(
                 "[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text)
             )
@@ -46,10 +53,9 @@ class FasterWhisperSTTHandler(BaseHandler):
 
         pred_text = " ".join(output_text).strip()
 
-        logger.debug("finished whisper inference")
         if pred_text:
             console.print(f"[yellow]USER: {pred_text}")
-
+            # the yield keyword is used to return transcription text iteratively, enabling the caller to process partial outputs in real-time
             yield pred_text
         else:
             logger.debug("no text detected. skipping...")
